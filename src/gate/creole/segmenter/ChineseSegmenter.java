54aa7af326b454718071d57e844c5c6964c1e292
Yu Zhihang <zhihangyu@gmail.com>
2015-05-12 16:17:58 +0800

    comment out sentence splitter

--- src/gate/creole/segmenter/ChineseSegmenter.java
+++ src/gate/creole/segmenter/ChineseSegmenter.java
@@ -3,6 +3,8 @@ package gate.creole.segmenter;
 
 import edu.stanford.nlp.util.CoreMap;
 import gate.AnnotationSet;
+import gate.Factory;
+import gate.FeatureMap;
 import gate.Resource;
 import gate.creole.AbstractLanguageAnalyser;
 import gate.creole.ExecutionException;
@@ -23,6 +25,7 @@ import java.net.URL;
 import java.text.NumberFormat;
 import java.util.List;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 import static edu.stanford.nlp.ling.CoreAnnotations.*;
 
@@ -42,7 +45,7 @@ public class ChineseSegmenter extends AbstractLanguageAnalyser {
 
     public static final String SPACE_LABEL = "spaceLabel";
 
-    public static final String SENTENCE_LABEL = "sentenceLabel";
+    public static final String Split_LABEL = "splitLabel";
 
     public static final String TOKEN_STRING_FEATURE = "string";
 
@@ -73,21 +76,27 @@ public class ChineseSegmenter extends AbstractLanguageAnalyser {
             fireProgressChanged(0);
 
             Properties props = new Properties();
-            props.setProperty("annotators", "segment ssplit");
+            props.setProperty("annotators", "segment");
             props.setProperty("customAnnotatorClass.segment", "edu.stanford.nlp.pipeline.ChineseSegmenterAnnotator");
             props.setProperty("segment.model", modelFile.toString().substring(5));
             props.setProperty("segment.sighanCorporaDict", dictDir.toString().substring(5));
             props.setProperty("segment.serDictionary", dictFile.toString().substring(5));
             props.setProperty("segment.sighanPostProcessing", "true");
-            props.setProperty("ssplit.boundaryTokenRegex", "[.]|[!?]+|[。]|[！？]+");
+            props.setProperty("segment.verbose", "false");
+//            props.setProperty("ssplit.boundaryTokenRegex", "[.]|[!?]+|[。]|[！？]+");
             StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-            String content = "";
+            String content = document.getContent().getContent(new Long(0), document.getContent().size()).toString();
 
-            try {
-                content = document.getContent().getContent(new Long(0), document.getContent().size()).toString();
-            } catch (Exception e) {
-                System.out.println("Document content offsets wrong: " + e);
-            }
+//            String[] paragrams = getParagrams(content);
+//            int paragramcount = paragrams.length;
+//            int paragramlengh = 0;
+//            for (int index = 0; index < paragramcount; index++) {
+//                String paragram = paragrams[index];
+//
+//                SetSplitFeatures(paragramlengh, paragram);
+//
+//                paragramlengh += paragram.length() + paragramflagLength;
+//            }
 
             Annotation doc = new Annotation(content);
             pipeline.annotate(doc);
@@ -95,43 +104,34 @@ public class ChineseSegmenter extends AbstractLanguageAnalyser {
             Long tokenStart;
             Long tokenEnd;
             Long prevTokenEnd = new Long(0);
-            List<CoreMap> sentences = doc.get(SentencesAnnotation.class);
 
-            for (CoreMap sentence : sentences) {
-                // traversing the words in the current sentence
-                // a CoreLabel is a CoreMap with additional token-specific methods
-//                try {
-//                    outputAS.add(sentenceStart, SentenceEnd, sentenceLabel, sentenceMap);
-//                } catch (InvalidOffsetException e) {
-//                    System.out.println("Token alignment problem:" + e);
-//                }
+            List<CoreLabel> tokens = doc.get(TokensAnnotation.class);
 
-                for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
-                    String word = token.get(TextAnnotation.class);
-                    tokenStart = new Long(token.get(CharacterOffsetBeginAnnotation.class));
-                    tokenEnd = new Long(token.get(CharacterOffsetEndAnnotation.class));
+            for (CoreLabel token : tokens) {
+                String word = token.get(TextAnnotation.class);
+                tokenStart = new Long(token.get(CharacterOffsetBeginAnnotation.class));
+                tokenEnd = new Long(token.get(CharacterOffsetEndAnnotation.class));
 
-                    SimpleFeatureMapImpl tokenMap = new SimpleFeatureMapImpl();
+                SimpleFeatureMapImpl tokenMap = new SimpleFeatureMapImpl();
 
-                    // add the token annotation
-                    try {
-                        tokenMap.put(TOKEN_STRING_FEATURE, word);
-                        outputAS.add(tokenStart, tokenEnd, tokenLabel, tokenMap);
-                    } catch (InvalidOffsetException e) {
-                        System.out.println("Token alignment problem:" + e);
-                    }
-
-                    // do we need to add a space annotation?
-                    if (tokenStart > prevTokenEnd) {
-                        try {
-                            outputAS.add(prevTokenEnd, tokenStart, "SpaceToken", new SimpleFeatureMapImpl());
-                        } catch (InvalidOffsetException e) {
-                            System.out.println("Space token alignment problem:" + e);
-                        }
-                    }
-
-                    prevTokenEnd = tokenEnd;
+                // add the token annotation
+                try {
+                    tokenMap.put(TOKEN_STRING_FEATURE, word);
+                    outputAS.add(tokenStart, tokenEnd, tokenLabel, tokenMap);
+                } catch (InvalidOffsetException e) {
+                    System.out.println("Token alignment problem:" + e);
                 }
+
+                // do we need to add a space annotation?
+                if (tokenStart > prevTokenEnd) {
+                    try {
+                        outputAS.add(prevTokenEnd, tokenStart, spaceLabel, new SimpleFeatureMapImpl());
+                    } catch (InvalidOffsetException e) {
+                        System.out.println("Space token alignment problem:" + e);
+                    }
+                }
+
+                prevTokenEnd = tokenEnd;
             }
 
             fireProcessFinished();
@@ -145,6 +145,53 @@ public class ChineseSegmenter extends AbstractLanguageAnalyser {
         }
     }
 
+    private String[] getParagrams(String content) {
+        String sentenceSplit = "";
+        // if the paragram flag is \r\n
+        if (content.contains("\r\n")) {
+            sentenceSplit = "\r\n";
+            paragramflagLength = 2;
+        } else {
+            sentenceSplit = "\n";
+            paragramflagLength = 1;
+        }
+        Pattern externalSplitsPattern = Pattern.compile(sentenceSplit);
+        String[] paragrams = externalSplitsPattern.split(content);
+        return paragrams;
+
+    }
+
+//    protected int getNoSpaceIdx(int startIdx) {
+//        String content = document.getContent().toString();
+//        if (startIdx >= content.length() - 1) {
+//            return 0;
+//        }
+//        String spacestring = content.substring(startIdx, startIdx + 1);
+//        while (spacestring != null && spacestring.equals(" ")) {
+//            startIdx++;
+//            spacestring = content.substring(startIdx, startIdx + 1);
+//        }
+//        return startIdx;
+//    }
+
+//    private boolean SetSplitFeatures(int paragramstart, String paragram) throws InvalidOffsetException {
+//        // get document's annotationset
+//        AnnotationSet annotationSet;
+//        if (outputASName == null ||
+//                outputASName.equals("")) annotationSet = document.getAnnotations();
+//        else annotationSet = document.getAnnotations(outputASName);
+//
+//        try {
+//            FeatureMap features = Factory.newFeatureMap();
+//            features.put("kind", "external");
+//            annotationSet.add(new Long(paragramstart + paragram.length()), new Long(paragramstart + paragram.length() + paragramflagLength),
+//                    "Split", features);
+//        } catch (InvalidOffsetException ioe) {
+//            return false;
+//        }
+//
+//        return true;
+//    }
 
     public void setEncoding(String encoding) {
         this.encoding = encoding;
@@ -174,17 +221,6 @@ public class ChineseSegmenter extends AbstractLanguageAnalyser {
     @CreoleParameter(comment = "Output annotation set name", defaultValue = "")
     public void setOutputASName(String outputASName) {
         this.outputASName = outputASName;
-    }
-
-    public String getSentenceLabel() {
-        return this.sentenceLabel;
-    }
-
-    @Optional
-    @RunTime
-    @CreoleParameter(comment = "Annotation type for sentencess", defaultValue = "Sentence")
-    public void setSentenceLabel(String sentenceLabel) {
-        this.sentenceLabel = sentenceLabel;
     }
 
     public String getTokenLabel() {
@@ -236,17 +272,28 @@ public class ChineseSegmenter extends AbstractLanguageAnalyser {
         return this.dictDir;
     }
 
+//    @Optional
+//    @RunTime
+//    @CreoleParameter(comment = "Annotation type for spaces", defaultValue = "Split")
+//    public void setSplitLabel(String splitLabel) {
+//        this.splitLabel = splitLabel;
+//    }
+//
+//    public String getSplitLabel() {
+//        return this.splitLabel;
+//    }
+
     private String inputASName;
 
     private String encoding;
 
     private String outputASName;
 
-    private String sentenceLabel;
-
     private String tokenLabel;
 
     private String spaceLabel;
+
+//    private String splitLabel;
 
     private URL modelFile;
 
@@ -254,5 +301,6 @@ public class ChineseSegmenter extends AbstractLanguageAnalyser {
 
     private URL dictDir;
 
-}
+    private int paragramflagLength = 1;
 
+}
